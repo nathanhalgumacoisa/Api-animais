be51fa6fdab4788c635c3a156fc9fd31b2927c4a
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-11-16 10:42:48 -0300

    nova feat: chamaAnimais e chamarAnimal

--- src/controllers/animal.controller.js
+++ src/controllers/animal.controller.js
@@ -2,3 +2,20 @@ import { AnimalList } from "../models/animals/AnimalList.js";
 import { Animal } from "../models/animals/Animal.js";
 
 const listaAnimais = new AnimalList
+
+export const chamarAnimais = (req, res) => {
+    const animais = listaAnimais.chamarAnimais();
+
+    if(animais.length){
+        return res.status(200).json(animais);
+    }
+    return res.status(404).json({message: "não a animais cadastrados"});
+}
+
+export const chamarAnimal = (req, res) => {
+    const { id } = req.params;
+    const animal = listaAnimais.chamarAnimalPeloId(id);
+
+    if (!animal) res.status(404).json({message: "animal não encontrado"});
+        res.status(200).json(animal)
+}
